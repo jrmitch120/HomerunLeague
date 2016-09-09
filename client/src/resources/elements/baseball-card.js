1829d0063000482800ab62f815d7a94dc8b35ff9
Jeff Mitchell <jrmitch120@yahoo.com>
2016-09-09 16:15:53 -0500

    Flipping cards

--- client/src/resources/elements/baseball-card.js
+++ client/src/resources/elements/baseball-card.js
@@ -1,11 +1,18 @@
-import {bindable} from 'aurelia-framework'
+import {bindable} from 'aurelia-framework';
 import $ from 'bootstrap';
+import 'flip';
 
 export class BaseballCard {
     @bindable id = '';
     @bindable player = {};
 
+    attached() {
+        //console.info(this.id);
+        $(`#${this.id}`).flip({trigger: 'click'}); 
+    }
+
     flipCard() {
+
         console.info(this.id);
     }
 }
