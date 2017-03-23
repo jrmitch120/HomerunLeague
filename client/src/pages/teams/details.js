34f90b05b2dea058e5b4ce85c7149d8fe5ea6acd
Jeff Mitchell <jrmitch120@yahoo.com>
2017-03-23 15:28:02 -0500

    Welcome + team details

--- client/src/pages/teams/details.js
+++ client/src/pages/teams/details.js
@@ -3,9 +3,10 @@ import { Api } from '../../services/api';
 
 @inject(Api)
 export class Create {
-
   _api;
+
   team;
+  registrationOpen;
 
   constructor(api) {
     this._api = api;
@@ -14,14 +15,21 @@ export class Create {
   activate(params, routeConfig) {
     this.routeConfig = routeConfig;
 
-    return this._api.getTeam(params.id).then(response => {
-      if (response.team) {
-        this.team = response.team;
-        this.routeConfig.navModel.setTitle(this.team.name);
-        console.info(this.team);
+    // See if registration is open, skip team load if we're currently in open enrollment.
+    return this._api.getSettings().then(response => {
+      this.registrationOpen = response.registrationOpen;
+
+      // Only fetch team information if registration is closed
+      if (!this.registrationOpen) {
+        return this._api.getTeam(params.id).then(response => {
+          if (response.team) {
+            this.team = response.team;
+            this.routeConfig.navModel.setTitle(this.team.name);
+          }
+          else
+            this.routeConfig.navModel.setTitle('Team Not Found');
+        });
       }
-      else
-        this.routeConfig.navModel.setTitle('Team Not Found');
     });
   }
 }
--- client/src/pages/welcome/index.js
+++ client/src/pages/welcome/index.js
@@ -1,5 +1,11 @@
-export class Welcome {
-	constructor() {
+import {inject} from 'aurelia-framework';
+import {Api} from '../../services/api';
 
+@inject(Api)
+export class Welcome {
+	year;
+
+	constructor(api) {
+		this.year = api.year;
 	}
 }
